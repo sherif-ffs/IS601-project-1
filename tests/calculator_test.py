2fb190079cee10185bd136799dc4386ee0f0fc9f
Sherif Elmetwally <sge5@njit.edu>
2020-02-27 11:31:31 -0500

    FEATURE: add test case for subtract method

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -7,3 +7,14 @@ def test_calc_add_fail():
     from calculator import Calculator
     calc = Calculator()
     assert calc.add(5,2) != 3
+
+def test_calc_subtract():
+    from calculator import Calculator
+    calc = Calculator()
+    assert calc.subtract(6,3) == 3
+
+def test_calc_subtract_fail():
+    from calculator import Calculator
+    calc = Calculator()
+    assert calc.subtract(5,2) != 5
+
